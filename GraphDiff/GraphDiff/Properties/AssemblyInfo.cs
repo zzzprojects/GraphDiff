d909f5f77d6f4640f896daee25d1f997025492ec
refactorthis <brentmckendrick@gmail.com>
2013-07-14 03:34:05 +1000

    Updated nuget and DLL versioning info.

--- GraphDiff/GraphDiff/Properties/AssemblyInfo.cs
+++ GraphDiff/GraphDiff/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DetachedUpdate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GraphDiff")]
+[assembly: AssemblyDescription("Detached graph auto update for entity framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DetachedUpdate")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("GraphDiff")]
+[assembly: AssemblyCopyright("Copyright © Brent McKendrick 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.4")]
