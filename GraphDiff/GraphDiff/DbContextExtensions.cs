fbd498fbae8948d636c05a0f853c24e1cf827d66
andyp <andy.pelzer@gmx.de>
2014-01-04 14:29:18 +0100

    Moved some methods to extensions region

--- GraphDiff/GraphDiff/DbContextExtensions.cs
+++ GraphDiff/GraphDiff/DbContextExtensions.cs
@@ -4,7 +4,6 @@
  * License MIT (c) Brent McKendrick 2012
  */
 
-using RefactorThis.GraphDiff.Internal;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,6 +14,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using RefactorThis.GraphDiff.Internal;
 
 namespace RefactorThis.GraphDiff
 {
@@ -187,12 +187,12 @@ namespace RefactorThis.GraphDiff
 	        if (member.IsOwned)
 	        {
 	            // Check if the same key, if so then update values on the entity
-	            if (IsKeyIdentical(context, newvalue, dbvalue))
+                if (IsKeyIdentical(context, newvalue, dbvalue))
 	                context.UpdateValuesWithConcurrencyCheck(newvalue, dbvalue);
 	            else
 	                member.Accessor.SetValue(dataStoreEntity, newvalue, null);
 
-	            AttachCyclicNavigationProperty(context, dataStoreEntity, newvalue);
+                AttachCyclicNavigationProperty(context, dataStoreEntity, newvalue);
 
 	            foreach (var childMember in member.Members)
 	                RecursiveGraphUpdate(context, dbvalue, newvalue, childMember);
@@ -206,7 +206,7 @@ namespace RefactorThis.GraphDiff
 	            }
 
 	            // do nothing if the key is already identical
-	            if (IsKeyIdentical(context, newvalue, dbvalue))
+                if (IsKeyIdentical(context, newvalue, dbvalue))
 	                return;
 
                 context.AttachAndReloadEntity(newvalue);
@@ -215,128 +215,99 @@ namespace RefactorThis.GraphDiff
 	        }
 	    }
 
-	    private static bool IsKeyIdentical(DbContext context, object newValue, object dbValue)
-	    {
-	        if (newValue == null || dbValue == null)
-	            return false;
-
-	        var keyFields = context.GetPrimaryKeyFieldsFor(ObjectContext.GetObjectType(newValue.GetType()));
-	        return CreateHash(keyFields, newValue) == CreateHash(keyFields, dbValue);
-	    }
-
-	    private static void AttachAndReloadEntity(this DbContext context, object entity)
-	    {
-            if (context.Entry(entity).State == EntityState.Detached)
-                context.Set(ObjectContext.GetObjectType(entity.GetType())).Attach(entity);
-
-	        if (GraphDiffConfiguration.ReloadAssociatedEntitiesWhenAttached)
-	            context.Entry(entity).Reload();
-	    }
-
-	    private static string CreateHash(IEnumerable<PropertyInfo> keys, object entity)
-        {
-            // Create unique string representing the keys
-            string code = "";
-
-            foreach (var property in keys)
-                code += "|" + property.GetValue(entity, null).GetHashCode();
-
-            return code;
-        }
-
 	    #endregion
 
-		#region Extensions
+        #region Extensions
 
-		// attaches the navigation property of a child back to its parent (if exists)
-	    private static void AttachCyclicNavigationProperty(this IObjectContextAdapter context, object parent, object child)
-		{
-			if (parent == null || child == null)
-				return;
+        // attaches the navigation property of a child back to its parent (if exists)
+        private static void AttachCyclicNavigationProperty(this IObjectContextAdapter context, object parent, object child)
+        {
+            if (parent == null || child == null)
+                return;
 
-			var parentType = ObjectContext.GetObjectType(parent.GetType());
-			var childType = ObjectContext.GetObjectType(child.GetType());
+            var parentType = ObjectContext.GetObjectType(parent.GetType());
+            var childType = ObjectContext.GetObjectType(child.GetType());
 
-	        var navigationProperties = context.ObjectContext.MetadataWorkspace
-	                .GetItems<EntityType>(DataSpace.OSpace)
-	                .Single(p => p.FullName == childType.FullName)
-	                .NavigationProperties;
+            var navigationProperties = context.ObjectContext.MetadataWorkspace
+                    .GetItems<EntityType>(DataSpace.OSpace)
+                    .Single(p => p.FullName == childType.FullName)
+                    .NavigationProperties;
 
-	        var parentNavigationProperty = navigationProperties
-	                .Where(navigation => navigation.TypeUsage.EdmType.Name == parentType.Name)
-	                .Select(navigation => childType.GetProperty(navigation.Name))
-	                .FirstOrDefault();
+            var parentNavigationProperty = navigationProperties
+                    .Where(navigation => navigation.TypeUsage.EdmType.Name == parentType.Name)
+                    .Select(navigation => childType.GetProperty(navigation.Name))
+                    .FirstOrDefault();
 
             if (parentNavigationProperty != null)
                 parentNavigationProperty.SetValue(child, parent, null);
-		}
+        }
 
 	    private static void UpdateValuesWithConcurrencyCheck<T>(this DbContext context, T from, T to) where T : class
 	    {
-            context.EnsureConcurrency(from, to);
-            context.Entry(to).CurrentValues.SetValues(from);
+	        context.EnsureConcurrency(from, to);
+	        context.Entry(to).CurrentValues.SetValues(from);
 	    }
 
         // Ensures concurrency properties are checked (manual at the moment.. todo)
 	    private static void EnsureConcurrency<T>(this IObjectContextAdapter db, T from, T to)
-        {
-            // get concurrency properties of T
-            var entityType = ObjectContext.GetObjectType(from.GetType());
-            var metadata = db.ObjectContext.MetadataWorkspace;
+	    {
+	        // get concurrency properties of T
+	        var entityType = ObjectContext.GetObjectType(from.GetType());
+	        var metadata = db.ObjectContext.MetadataWorkspace;
 
-            var objType = metadata.GetItems<EntityType>(DataSpace.OSpace).Single(p => p.FullName == entityType.FullName);
+	        var objType = metadata.GetItems<EntityType>(DataSpace.OSpace).Single(p => p.FullName == entityType.FullName);
 
-            // need internal string, code smells bad.. any better way to do this?
-            var cTypeName = (string) objType.GetType()
-                    .GetProperty("CSpaceTypeName", BindingFlags.Instance | BindingFlags.NonPublic)
-                    .GetValue(objType, null);
+	        // need internal string, code smells bad.. any better way to do this?
+	        var cTypeName = (string) objType.GetType()
+	                .GetProperty("CSpaceTypeName", BindingFlags.Instance | BindingFlags.NonPublic)
+	                .GetValue(objType, null);
 
-            var conceptualType = metadata.GetItems<EntityType>(DataSpace.CSpace).Single(p => p.FullName == cTypeName);
-            var concurrencyProperties = conceptualType.Members
-                    .Where(member => member.TypeUsage.Facets.Any(facet => facet.Name == "ConcurrencyMode" && (ConcurrencyMode) facet.Value == ConcurrencyMode.Fixed))
-                    .Select(member => entityType.GetProperty(member.Name))
-                    .ToList();
+	        var conceptualType = metadata.GetItems<EntityType>(DataSpace.CSpace).Single(p => p.FullName == cTypeName);
+	        var concurrencyProperties = conceptualType.Members
+                    .Where(member => member.TypeUsage.Facets.Any(facet => facet.Name == "ConcurrencyMode" && (ConcurrencyMode)facet.Value == ConcurrencyMode.Fixed))
+	                .Select(member => entityType.GetProperty(member.Name))
+	                .ToList();
 
-            // Check if concurrency properties are equal
-            // TODO EF should do this automatically should it not?
-            foreach (PropertyInfo concurrencyProp in concurrencyProperties)
-            {
-                // if is byte[] use array comparison, else equals().
+	        // Check if concurrency properties are equal
+	        // TODO EF should do this automatically should it not?
+	        foreach (PropertyInfo concurrencyProp in concurrencyProperties)
+	        {
+	            // if is byte[] use array comparison, else equals().
                 if ((concurrencyProp.PropertyType == typeof(byte[]) && !((byte[])concurrencyProp.GetValue(from, null)).SequenceEqual((byte[])concurrencyProp.GetValue(to, null)))
                     || concurrencyProp.GetValue(from, null).Equals(concurrencyProp.GetValue(to, null)))
-                {
-                    throw new DbUpdateConcurrencyException(String.Format("{0} failed optimistic concurrency", concurrencyProp.Name));
-                }  
-            }
-        }
+	            {
+	                throw new DbUpdateConcurrencyException(String.Format("{0} failed optimistic concurrency", concurrencyProp.Name));
+	            }
+	        }
+	    }
 
-        private static T FindEntityMatching<T>(this DbContext context, T entity, params string[] includes) where T : class
-        {
-            // attach includes to IQueryable
-            var query = context.Set<T>().AsQueryable();
-            foreach (var include in includes)
-                query = query.Include(include);
+	    private static T FindEntityMatching<T>(this DbContext context, T entity, params string[] includes) where T : class
+	    {
+	        // attach includes to IQueryable
+	        var query = context.Set<T>().AsQueryable();
+	        foreach (var include in includes)
+	            query = query.Include(include);
 
-            // Run the find operation
-            return query.SingleOrDefault(context.CreateKeyPredicateExpression(entity));
-        }
+	        // Run the find operation
+	        return query.SingleOrDefault(context.CreateKeyPredicateExpression(entity));
+	    }
 
-        private static Expression<Func<T, bool>> CreateKeyPredicateExpression<T>(this IObjectContextAdapter context, T entity) where T : class
-        {
-            // get key properties of T
-            var keyProperties = context.GetPrimaryKeyFieldsFor(typeof(T)).ToList();
+	    private static Expression<Func<T, bool>> CreateKeyPredicateExpression<T>(this IObjectContextAdapter context, T entity) where T : class
+	    {
+	        // get key properties of T
+	        var keyProperties = context.GetPrimaryKeyFieldsFor(typeof(T)).ToList();
 
-            ParameterExpression parameter = Expression.Parameter(typeof(T));
-            Expression expression = CreateEqualsExpression(entity, keyProperties[0], parameter);
-            for (int i = 1; i < keyProperties.Count; i++)
-                expression = Expression.And(expression, CreateEqualsExpression(entity, keyProperties[i], parameter));
-            return Expression.Lambda<Func<T, bool>>(expression, parameter);
-        }
+	        ParameterExpression parameter = Expression.Parameter(typeof(T));
+	        Expression expression = CreateEqualsExpression(entity, keyProperties[0], parameter);
+	        for (int i = 1; i < keyProperties.Count; i++)
+	            expression = Expression.And(expression, CreateEqualsExpression(entity, keyProperties[i], parameter));
+	        return Expression.Lambda<Func<T, bool>>(expression, parameter);
+	    }
 
-        private static Expression CreateEqualsExpression(object entity, PropertyInfo keyProperty, Expression parameter)
-        {
-            return Expression.Equal(Expression.Property(parameter, keyProperty), Expression.Constant(keyProperty.GetValue(entity, null)));
-        }
+	    private static Expression CreateEqualsExpression(object entity, PropertyInfo keyProperty, Expression parameter)
+	    {
+	        return Expression.Equal(Expression.Property(parameter, keyProperty), Expression.Constant(keyProperty.GetValue(entity, null)));
+	    }
 
 	    private static List<PropertyInfo> GetPrimaryKeyFieldsFor(this IObjectContextAdapter db, Type entityType)
 	    {
@@ -348,7 +319,35 @@ namespace RefactorThis.GraphDiff
 	        return keyMembers.Select(k => entityType.GetProperty(k.Name)).ToList();
 	    }
 
-	    #endregion
+	    private static bool IsKeyIdentical(this IObjectContextAdapter context, object newValue, object dbValue)
+	    {
+	        if (newValue == null || dbValue == null)
+	            return false;
 
+	        var keyFields = context.GetPrimaryKeyFieldsFor(ObjectContext.GetObjectType(newValue.GetType()));
+	        return CreateHash(keyFields, newValue) == CreateHash(keyFields, dbValue);
+	    }
+
+	    private static void AttachAndReloadEntity(this DbContext context, object entity)
+	    {
+	        if (context.Entry(entity).State == EntityState.Detached)
+	            context.Set(ObjectContext.GetObjectType(entity.GetType())).Attach(entity);
+
+	        if (GraphDiffConfiguration.ReloadAssociatedEntitiesWhenAttached)
+	            context.Entry(entity).Reload();
+	    }
+
+	    private static string CreateHash(IEnumerable<PropertyInfo> keys, object entity)
+	    {
+	        // Create unique string representing the keys
+	        string code = "";
+
+	        foreach (var property in keys)
+	            code += "|" + property.GetValue(entity, null).GetHashCode();
+
+	        return code;
+        }
+
+        #endregion
 	}
 }
