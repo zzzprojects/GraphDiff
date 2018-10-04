ea088deeeae31087dd0a9377fe5c6f8a34a0d8ed
Lempireqc <34109409+Lempireqc@users.noreply.github.com>
2018-10-04 14:13:07 -0400

    Update DbContextExtensions.cs

--- GraphDiff/GraphDiff/DbContextExtensions.cs
+++ GraphDiff/GraphDiff/DbContextExtensions.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This code is provided as is with no warranty. If you find a bug please report it on github.
  * If you would like to use the code please leave this comment at the top of the page
  * License MIT (c) Brent McKendrick 2012
@@ -12,25 +12,27 @@ using RefactorThis.GraphDiff.Internal.Graph;
 
 namespace RefactorThis.GraphDiff
 {
-	public static class DbContextExtensions
-	{
+    public static class DbContextExtensions
+    {
         /// <summary>
-        /// Merges a graph of entities with the data store.
+        ///     Merges a graph of entities with the data store.
         /// </summary>
         /// <typeparam name="T">The type of the root entity</typeparam>
         /// <param name="context">The database context to attach / detach.</param>
         /// <param name="entity">The root entity.</param>
         /// <param name="mapping">The mapping configuration to define the bounds of the graph</param>
-	/// <param name="allowDelete">NEED TEXTE!!!!</param>
+        /// <param name="allowDelete">NEED TEXTE!!!!</param>
         /// <returns>The attached entity graph</returns>
-	    public static T UpdateGraph<T>(this DbContext context, T entity, Expression<Func<IUpdateConfiguration<T>, object>> mapping = null, bool allowDelete = true) where T : class, new()
-	    {
-           	var root = mapping == null ? new GraphNode() : new ConfigurationVisitor<T>().GetNodes(mapping);
-		root.AllowDelete = allowDelete;
-          	var graphDiffer = new GraphDiffer<T>(root);
-          	return graphDiffer.Merge(context, entity);
-	    }
+        public static T UpdateGraph<T>(this DbContext context, T entity,
+            Expression<Func<IUpdateConfiguration<T>, object>> mapping = null, bool allowDelete = true)
+            where T : class, new()
+        {
+            var root = mapping == null ? new GraphNode() : new ConfigurationVisitor<T>().GetNodes(mapping);
+            root.AllowDelete = allowDelete;
+            var graphDiffer = new GraphDiffer<T>(root);
+            return graphDiffer.Merge(context, entity);
+        }
 
         // TODO add IEnumerable<T> entities
-	}
+    }
 }
