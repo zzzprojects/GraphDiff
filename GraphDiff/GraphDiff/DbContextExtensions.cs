8c1bc0dc20a006dc381b50c7c76f69e1b49a5c6b
Jonathan Magnan <info@zzzprojects.com>
2018-10-08 13:15:02 -0400

    Update text + add extensions
    Update text + add extensions

--- GraphDiff/GraphDiff/DbContextExtensions.cs
+++ GraphDiff/GraphDiff/DbContextExtensions.cs
@@ -21,7 +21,7 @@ namespace RefactorThis.GraphDiff
         /// <param name="context">The database context to attach / detach.</param>
         /// <param name="entity">The root entity.</param>
         /// <param name="mapping">The mapping configuration to define the bounds of the graph</param>
-        /// <param name="allowDelete">NEED TEXTE!!!!</param>
+        /// <param name="allowDelete">Allow to delete from graph.</param>
         /// <returns>The attached entity graph</returns>
         public static T UpdateGraph<T>(this DbContext context, T entity,
             Expression<Func<IUpdateConfiguration<T>, object>> mapping = null, bool allowDelete = true)
@@ -31,6 +31,20 @@ namespace RefactorThis.GraphDiff
             root.AllowDelete = allowDelete;
             var graphDiffer = new GraphDiffer<T>(root);
             return graphDiffer.Merge(context, entity);
+        }
+
+        /// <summary>
+        ///     Merges a graph of entities with the data store.
+        /// </summary>
+        /// <typeparam name="T">The type of the root entity</typeparam>
+        /// <param name="context">The database context to attach / detach.</param>
+        /// <param name="entity">The root entity.</param>
+        /// <param name="allowDelete">Allow to delete from graph.</param>
+        /// <returns>The attached entity graph</returns>
+        public static T UpdateGraph<T>(this DbContext context, T entity, bool allowDelete)
+            where T : class, new()
+        {
+            return context.UpdateGraph(entity, null, allowDelete);
         }
 
         // TODO add IEnumerable<T> entities
